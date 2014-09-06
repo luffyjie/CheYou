dd90ec52fb26936e3c33ae74c9c10e78c61e2337
lujie <lujie2012@163.com>
2014-09-06 17:58:23 +0800

    1.集成了mjreresh 刷新

--- CheYou/CheYou/CheYouViewController.m
+++ CheYou/CheYou/CheYouViewController.m
@@ -13,6 +13,9 @@
 #import "UIImageView+MJWebCache.h"
 #import "MJPhotoBrowser.h"
 #import "MJPhoto.h"
+#import "MJRefresh.h"
+
+NSString *const MJTableViewCellIdentifier = @"sconddentifier";
 
 @interface CheYouViewController ()
 @property (nonatomic, strong) UIImageView *scroll;
@@ -33,22 +36,10 @@
     //设置吐槽tale
     self.tableView.backgroundColor = [LuJieCommon UIColorFromRGB:0xF2F2F2];
     self.tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0,0,0,10)];
-//    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    self.tableView.showsVerticalScrollIndicator = NO;
-    
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    //刷新获取数据
+    [self refreshConfig];
 }
-
- - (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-//    self.scroll.hidden = NO;
-}
-
-- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
-{
-//    self.scroll.hidden = YES;
-}
-
-
 
 - (void)didReceiveMemoryWarning
 {
@@ -72,6 +63,26 @@
 		}
 		[_tuCaoList addObject:tucao];
 	}
+}
+
+#pragma refresh config
+
+-(void)refreshConfig
+{
+//    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:MJTableViewCellIdentifier];
+    
+    self.tableView.showsVerticalScrollIndicator = NO;
+    [self.tableView addHeaderWithTarget:self action:@selector(headerRereshing)];
+    // 设置文字(也可以不设置,默认的文字在MJRefreshConst中修改)
+    self.tableView.headerPullToRefreshText = @"下拉可以刷新了";
+    self.tableView.headerReleaseToRefreshText = @"松开马上刷新了";
+    self.tableView.headerRefreshingText = @"正在刷新中...";
+    
+    // 2.上拉加载更多(进入刷新状态就会调用self的footerRereshing)
+    [self.tableView addFooterWithTarget:self action:@selector(footerRereshing)];
+    self.tableView.footerPullToRefreshText = @"上拉可以加载更多数据了";
+    self.tableView.footerReleaseToRefreshText = @"松开马上加载更多数据了";
+    self.tableView.footerRefreshingText = @"正在刷新中...";
 }
 
 #pragma mark - UITableViewDataSource
@@ -208,6 +219,50 @@
     browser.currentPhotoIndex = (tap.view.tag%10); // 弹出相册时显示的第一张图片是？
     browser.photos = photos; // 设置所有的图片
     [browser show];
+}
+
+/**
+ 为了保证内部不泄露，在dealloc中释放占用的内存
+ */
+- (void)dealloc
+{
+    NSLog(@"MJTableViewController--dealloc---");
+}
+
+
+#pragma mark 开始进入刷新状态
+- (void)headerRereshing
+{
+    // 1.添加假数据
+    for (NSInteger i = 0; i<5; i++) {
+        [_tuCaoList addObject:[_tuCaoList objectAtIndex:i]];
+    }
+    
+    // 2.2秒后刷新表格UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView headerEndRefreshing];
+    });
+}
+
+- (void)footerRereshing
+{
+    // 1.添加假数据
+    for (NSInteger i = 0; i<5; i++) {
+        [_tuCaoList addObject:[_tuCaoList objectAtIndex:i]];
+    }
+    
+    // 2.2秒后刷新表格UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView footerEndRefreshing];
+    });
 }
 
 @end
